a6158d2e2eec0ed1f3974f453b34a498c523b40b
mahtabjeihani@gmail.com <mahtabjeihani@gmail.com>
2024-07-11 13:35:35 +0330

    added the last phase of the project

---
+++ FlappyBird with DQN/utils.py
@@ -0,0 +1,97 @@
+import sys
+import inspect
+import heapq
+import random
+import io
+import functools
+
+def sign(x):
+    """
+    Returns 1 or -1 depending on the sign of x
+    """
+    if(x >= 0):
+        return 1
+    else:
+        return -1
+
+class Counter(dict):
+    def __getitem__(self, idx):
+        self.setdefault(idx, 0)
+        return dict.__getitem__(self, idx)
+
+    def incrementAll(self, keys, count):
+        for key in keys:
+            self[key] += count
+
+    def argMax(self):
+        if len(list(self.keys())) == 0:
+            return None
+        all = list(self.items())
+        values = [x[1] for x in all]
+        maxIndex = values.index(max(values))
+        return all[maxIndex][0]
+
+    def sortedKeys(self):
+        sortedItems = list(self.items())
+        def compare(x, y): return sign(y[1] - x[1])
+        sortedItems.sort(key=functools.cmp_to_key(compare))
+        return [x[0] for x in sortedItems]
+
+    def totalCount(self):
+        return sum(self.values())
+
+    def normalize(self):
+        total = float(self.totalCount())
+        if total == 0:
+            return
+        for key in list(self.keys()):
+            self[key] = self[key] / total
+
+    def divideAll(self, divisor):
+        divisor = float(divisor)
+        for key in self:
+            self[key] /= divisor
+
+    def copy(self):
+        return Counter(dict.copy(self))
+
+    def __mul__(self, y):
+        sum = 0
+        x = self
+        if len(x) > len(y):
+            x, y = y, x
+        for key in x:
+            if key not in y:
+                continue
+            sum += x[key] * y[key]
+        return sum
+
+    def __radd__(self, y):
+        for key, value in list(y.items()):
+            self[key] += value
+
+    def __add__(self, y):
+        addend = Counter()
+        for key in self:
+            if key in y:
+                addend[key] = self[key] + y[key]
+            else:
+                addend[key] = self[key]
+        for key in y:
+            if key in self:
+                continue
+            addend[key] = y[key]
+        return addend
+
+    def __sub__(self, y):
+        addend = Counter()
+        for key in self:
+            if key in y:
+                addend[key] = self[key] - y[key]
+            else:
+                addend[key] = self[key]
+        for key in y:
+            if key in self:
+                continue
+            addend[key] = -1 * y[key]
+        return addend
